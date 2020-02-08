2333f7400ef513e1ac851566f12d7f95107d7c42
Pednick <peterbozickovic@gmail.com>
2020-02-08 12:58:39 -0500

    Add files via upload

---
+++ gentoo chroot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+
+chroot /mnt/gentoo /bin/bash
+source /etc/profile
+export PS1="(chroot) ${PS1}"
+
